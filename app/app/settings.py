937b96531bed6b840643098165667a3d00fcd6ec
rojsiroj <sirojudin.dev@gmail.com>
2024-08-08 14:48:13 +0700

    config: enable redis chaching on django

--- app/app/settings.py
+++ app/app/settings.py
@@ -137,3 +137,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "core.User"
 
 REST_FRAMEWORK = {"DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema"}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
