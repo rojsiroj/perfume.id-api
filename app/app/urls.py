224028a9b641ac7afa84603af94f9a245eca7eb2
rojsiroj <sirojudin.dev@gmail.com>
2024-08-09 15:30:10 +0700

    config: add auto redirect to api docs

--- app/app/urls.py
+++ app/app/urls.py
@@ -8,9 +8,11 @@ from drf_spectacular.views import (
 )
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="api/docs")),
     path("admin/", admin.site.urls),
     path("api/schema", SpectacularAPIView.as_view(), name="api-schema"),
     path(
